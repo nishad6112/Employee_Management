948323588206a1aa1309445f39402062150cf149
vy211 <vipinkumar.y@simplify3x.com>
2024-03-10 16:11:35 +0530

    loginnew

--- client/src/component/dashboard/Table.jsx
+++ client/src/component/dashboard/Table.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 const Table = () => {
     return (
-        <div className="table_data">
-            <h1>Table Data</h1>
-        </div>
+        <div className="contain-table">
+      <table className="striped-table">
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Salary</th>
+            <th>Date</th>
+            <th colSpan={2} className="text-center">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        
+          
+      </table>
+    </div>
     );
 }
 
--- client/src/component/login/Login.jsx
+++ client/src/component/login/Login.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Header from '../dashboard/Header';
 
 const Login = () => {
     // State to hold login credentials
@@ -9,39 +7,41 @@ const Login = () => {
     const [password, setPassword] = useState('');
 
     // Function to handle form submission
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Add your login logic here
-        console.log("Email:", email);
-        console.log("Password:", password);
-    };
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     // Add your login logic here
+    //     console.log("Email:", email);
+    //     console.log("Password:", password);
+    // };
 
-    return (
-        <div className="container ">
-            <div className="row justify-content-center ">
-                <div className="col-md-6 ">
-                    <h2 className="text-center mb-4 ">Login</h2>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        </Form.Group>
-
-                        <Button className="mt-3" variant="dark" type="submit"  block>
-                            Submit
-                        </Button>
-                    </Form>
-                </div>
-            </div>
-        </div>
+  return (
+    <div>
+      <Header/>
+        <div className="small-container">
+      <form>
+        <h1>Login</h1>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="admin@example.com"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="qwerty"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <input style={{ marginTop: '12px' }} type="submit" value="Login" />
+      </form>
+      </div>
+      </div>
     );
 };
 
